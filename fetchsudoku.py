2766985ba77ee3301380b6d1dbf10ecff124ad06
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2016-09-14 12:02:00 +0200

    script for fetching sudoku instaces

---
+++ fetchsudoku.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print "Usage: "+sys.argv[0]+" difficulty_level"
+        sys.exit(2)
+
+    r = requests.get('http://www.thonky.com/sandbox/new-sudoku/ThonkySudokuJavascript.js?puzzle_id=&difficulty='+sys.argv[1])
+    if r.status_code != requests.codes.ok:
+        print "ERROR"
+        exit(2)
+    print r.text.split()[3][:-1]
+
+if __name__ == "__main__":
+    main()
