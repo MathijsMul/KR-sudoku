ae47d4622b8d7cca6abeb2c97093aea775440590
MathijsMul <Mathijs@wcw-stud-145-109-23-157.wireless.uva.nl>
2016-09-20 14:12:18 +0200

    changed strategies

--- lib/strategies.py
+++ lib/strategies.py
@@ -1,24 +1,22 @@
+import itertools
+
 def variable(r,c,v):
     return r * 81 + c * 9 + v
 
-def row_to_column():
-    print 'c row_to_column'
-    for r1 in range(0,9):
-        for c in range(0,9):
+def complete_row():
+    print 'c complete_row'
+    for r in range(0,9):
+        for c1 in range(0,9):
+            other_columns = filter(lambda x: x != c1, range(0,9))
             for v in range(1,10):
-                for r2 in filter(lambda x: x != r1, range(0,9)):
-                    print str(-1 * variable(r2,c,v)),
-                    print str(-1 * variable(r1,c,v)),
+                other_values = filter(lambda x: x != v, range(1,10))
+                perms = itertools.permutations(other_values)
+                for permutation in perms:
+                    #print(permutation)
+                    count = 0
+                    for value in permutation:
+                        print str(-1 * variable(r,other_columns[count],value)),
+                        count += 1
+                    print str(variable(r,c1,v)),
                     print '0'
-
-def column_to_row():
-    print 'c column_to_row'
-    for c1 in range(0,9):
-        for r in range(0,9):
-            for v in range(1,10):
-                for c2 in filter(lambda x: x != c1, range(0,9)):
-                    print str(-1 * variable(r,c1,v)),
-                    print str(-1 * variable(r,c2,v)),
-                    print '0'
-
- 
+                    
